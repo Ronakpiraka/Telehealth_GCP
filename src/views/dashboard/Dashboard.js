20a05fcfe49a3a76eb46e9526a7487e0694108f5
Ronak Piraka <78247891+Ronakpiraka@users.noreply.github.com>
2022-11-16 23:02:28 +0530

    changes in dashboard not needed

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -24,86 +24,95 @@ import MainChartExample from '../charts/MainChartExample.js'
 const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
 // const WidgetsBrand = lazy(() => import('../widgets/WidgetsBrand.js'))
 
-const Dashboard = () => {
+
+export default function Dashboard (){
+const [dashdetails, setdashdetails] = React.useState([]);
+const fetchdashdetailsdata = () => {
+  console.log("check function")
+var requestOptions = {
+  method: 'GET'
+};
+
+fetch("https://us-central1-telehealth-365911.cloudfunctions.net/fetchDashboard", requestOptions)
+.then((resp) => resp.json())
+.then((response) => {
+  setdashdetails(response)
+  console.log(dashdetails)
+  
+  // console.log( eval(JSON.stringify(data)));
+})
+.catch(error => console.log('error', error));
+}
+// useEffect(() => { 
+//   // console.log("hello useeffect")
+//   fetchdashdetailsdata();
+// })
+
+// const Dashboard = () => {
   return (
-    <>
-      <WidgetsDropdown />
+    <div>
+      <WidgetsDropdown/>
       <CCard>
         <CCardBody>
+        
           <CRow>
             <CCol sm="5">
               <h3 id="traffic" className="card-title mb-0">Preventive Care</h3>
-              {/* <div className="small text-muted">September 2021</div> */}
             </CCol>
             <CCol sm="7" className="d-none d-md-block">
-              {/* <CButton color="primary" className="float-right">
-                <CIcon name="cil-cloud-download"/>
-              </CButton> */}
-              {/* <CButtonGroup className="float-right mr-3">
-                {
-                  ['Day', 'Month', 'Year'].map(value => (
-                    <CButton
-                      color="outline-secondary"
-                      key={value}
-                      className="mx-0"
-                      active={value === 'Month'}
-                    >
-                      {value}
-                    </CButton>
-                  ))
-                }
-              </CButtonGroup> */}
             </CCol>
           </CRow>
-          {/* <MainChartExample style={{height: '300px', marginTop: '40px'}}/> */}
+
           <Iframe width="100%" height="380px" src="https://datastudio.google.com/embed/reporting/c4611298-10ab-4b55-9625-33805ce06003/page/tEnnC" frameborder="0" style="border:0" allowfullscreen/>
         </CCardBody>
-      <CCardGroup className="mb-4">
-
-        <CWidgetProgressIcon
-          header="850"
-          text="New Patients"
-          color="gradient-info"
-          inverse
-        >
-          <CIcon name="cil-people" height="36"/>
-        </CWidgetProgressIcon>
-        <CWidgetProgressIcon
-          header="385"
-          text="Active Cases"
-          color="gradient-success"
-          inverse
-        >
-          <CIcon name="cil-userFollow" height="36"/>
-        </CWidgetProgressIcon>
-        <CWidgetProgressIcon
-          header="1238"
-          text="Facilities"
-          color="gradient-warning"
-          inverse
-        >
-          <CIcon name="cil-basket" height="36"/>
-        </CWidgetProgressIcon>
-        <CWidgetProgressIcon
-          header="280"
-          text="Care Insights"
-          color="gradient-primary"
-          inverse
-        >
-          <CIcon name="cil-chartPie" height="36"/>
-        </CWidgetProgressIcon>
-        <CWidgetProgressIcon
-          header="5250"
-          text="Insurance Code"
-          color="gradient-danger"
-          inverse
-        >
-          <CIcon name="cil-speedometer" height="36"/>
-        </CWidgetProgressIcon>
-      </CCardGroup>
+        {/* {dashdetails.length > 0 && dashdetails.map(item) => { */}
+          <CCardGroup className="mb-4">
+            <CWidgetProgressIcon
+            header ="250"
+              // header = {item.total_patient_count}
+              text="New Patients"
+              color="gradient-info"
+              inverse
+            >
+              <CIcon name="cil-people" height="36" />
+            </CWidgetProgressIcon><CWidgetProgressIcon
+            header ="250"
+              // header={item.active_count}
+              text="Active Cases"
+              color="gradient-success"
+              inverse
+            >
+                <CIcon name="cil-userFollow" height="36" />
+              </CWidgetProgressIcon><CWidgetProgressIcon
+                header ="250"
+                // header={item.Provider_count}
+                text="Facilities"
+                color="gradient-warning"
+                inverse
+              >
+                <CIcon name="cil-basket" height="36" />
+              </CWidgetProgressIcon><CWidgetProgressIcon
+                header ="250"
+                // header={item.care_insights_count}
+                text="Care Insights"
+                color="gradient-primary"
+                inverse
+              >
+                <CIcon name="cil-chartPie" height="36" />
+              </CWidgetProgressIcon><CWidgetProgressIcon
+                header ="250"
+                // header={item.claims_count}
+                text="Insurance Code"
+                color="gradient-danger"
+                inverse
+              >
+                <CIcon name="cil-speedometer" height="36" />
+              </CWidgetProgressIcon>
+          </CCardGroup>
+        {/* } */}
       </CCard>
-      </>
-  )
+      </div>)
+    
 }
+  
 
-export default Dashboard
