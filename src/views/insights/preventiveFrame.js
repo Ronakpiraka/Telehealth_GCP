aa3b107c5a9e4f595788762cb114469732afbf28
Aviraj Ranawat <Aviraj.Ranawat@BRILLIO.COM>
2022-11-21 17:20:03 +0530

    Preventive care view for false remotecarestatus

--- src/views/insights/preventiveFrame.js
+++ src/views/insights/preventiveFrame.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import Iframe from 'react-iframe'
+import {useLocation} from "react-router-dom";
+import {useEffect, useState} from 'react'
 
 const Preventive= () => {
+    
+    const location = useLocation();
+    var flags;
+    const [singlepatientid, setsinglepatientid] = useState('');
+
+    useEffect(() => {
+
+        flags = location.search.split('^')[1];
+    
+        console.log(flags)
+    
+        let singlepatientid = location.search.split('=')[1];
+    
+        console.log('-----------------singleorderid--------------------------------------')
+    
+        console.log(singlepatientid)
+    
+        console.log('-------------------------------------------------------')
+    
+        setsinglepatientid(singlepatientid)
+    
+    
+    
+    })
+    if(singlepatientid){
+        var insighturl = "https://datastudio.google.com/embed/reporting/c4611298-10ab-4b55-9625-33805ce06003/page/tEnnC?params=%7B%22df2%22:%22include%25EE%2580%25800%25EE%2580%2580IN%25EE%2580%2580"+singlepatientid+"%22%7D"
+    }
+    else
+    {
+        var insighturl = "https://datastudio.google.com/embed/reporting/c4611298-10ab-4b55-9625-33805ce06003/page/tEnnC"
+    }
+    
+
     return(
         <div>
-            <Iframe width="100%" height="380px" src="https://datastudio.google.com/embed/reporting/c4611298-10ab-4b55-9625-33805ce06003/page/tEnnC" frameborder="0" style="border:0" allowfullscreen/>
+            <Iframe width="100%" height="380px" src={insighturl} frameborder="0" style="border:0" allowfullscreen/>
         </div>
     )
 }
