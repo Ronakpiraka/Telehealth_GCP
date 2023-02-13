a13dec02d7c5b5aae9514bc5b792b11153d5a780
Fangs560 <aviraj.ranawat.is@gmail.com>
2023-02-13 16:58:04 +0530

    Fixed Preventive & Continuous care embed links

--- src/views/insights/continuousFrame.js
+++ src/views/insights/continuousFrame.js
@@ -17,11 +17,11 @@ const Continuous= () => {
     },[])
     if(singlepatientid)
     {    
-        var insighturl ="https://datastudio.google.com/embed/reporting/16901bed-1e96-44c2-82c4-b92d4797b0ac/page/tEnnC?params=%7B%22df4%22:%22include%25EE%2580%25800%25EE%2580%2580IN%25EE%2580%2580"+singlepatientid+"%22%7D"
+        var insighturl ="https://lookerstudio.google.com/embed/reporting/16901bed-1e96-44c2-82c4-b92d4797b0ac/page/tEnnC?params=%7B%22df4%22:%22include%25EE%2580%25800%25EE%2580%2580IN%25EE%2580%2580"+singlepatientid+"%22%7D"
     }
     else
     {
-        var insighturl = "https://datastudio.google.com/embed/reporting/c6041d77-a0b2-42dd-86da-6489602b5870/page/tEnnC"
+        var insighturl = "https://lookerstudio.google.com/embed/reporting/c6041d77-a0b2-42dd-86da-6489602b5870/page/tEnnC"
     }
     
     return(
--- src/views/insights/preventive.js
+++ src/views/insights/preventive.js
@@ -12,7 +12,7 @@ export default function Insights() {
   return (
     <div>
       <Charts/>
-      <Iframe width="100%" height="450px" src="https://datastudio.google.com/embed/reporting/c4611298-10ab-4b55-9625-33805ce06003/page/tEnnC" frameborder="0" style="border:0" allowfullscreen/>
+      <Iframe width="100%" height="450px" src="https://lookerstudio.google.com/embed/reporting/c4611298-10ab-4b55-9625-33805ce06003/page/tEnnC" frameborder="0" style="border:0" allowfullscreen/>
     </div>
   )
 }
