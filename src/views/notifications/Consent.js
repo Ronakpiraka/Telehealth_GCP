5f698b6b31aa7372f0ee781cbe31f8c03870e305
Subhangipatro <Subhangipatro999@gmail.com>
2023-02-27 14:09:21 +0530

    signature

--- src/views/notifications/Consent.js
+++ src/views/notifications/Consent.js
@@ -10,90 +10,16 @@ import { useHistory, useLocation } from "react-router-dom";
 import SignatureCanvas from 'react-signature-canvas';
 import ReactDOM from 'react-dom';
 import styles from './Consent.css'
-
+import Signature from './signature'
 
 export default function RadioButtonsGroup() {
-    state = {trimmedDataURL: null}
-    sigPad = {}
-    clear = () => {
-        this.sigPad.clear()
-    }
-    trim = () => {
-        this.setState({trimmedDataURL: this.sigPad.getTrimmedCanvas()
-            .toDataURL('image.png')})
-         }
+   
     const history = useHistory();
     const redirecttoConsent= () => {
         var url = `/notifications/email`;
         history.push(`${url}`);
      }
-    //  ReactDOM.render(
-    //     <SignatureCanvas penColor='green'
-    //       canvasProps={{width: 500, height: 200, className: 'sigCanvas'}} />,
-    //     document.getElementById('react-container')
-    //   )
-    //  const canvas = document.querySelector('canvas');
-    //  const form = document.querySelector('.signature-pad-form');
-    //  const clearButton = document.querySelector('.clear-button');
-    //  const ctx = canvas.getContext('2d');
-    //  let writingMode = false;
-    //  canvas.addEventListener('pointerdown', handlePointerDown, {passive: true});
-    //  canvas.addEventListener('pointerup', handlePointerUp, {passive: true});
-    //  canvas.addEventListener('pointermove', handlePointerMove, {passive: true});
-    //  const handlePointerDown = (event) => {
-    //     writingMode = true;
-    //     ctx.beginPath();
-    //     const [positionX, positionY] = getCursorPosition(event);
-    //     ctx.moveTo(positionX, positionY);
-    //   }
-    //   const handlePointerUp = () => {
-    //     writingMode = false;
-    //   }
-    //   const handlePointerMove = (event) => {
-    //     if (!writingMode) return
-    //     const [positionX, positionY] = getCursorPosition(event);
-    //     ctx.lineTo(positionX, positionY);
-    //     ctx.stroke();
-    //   }
-    //    const getCursorPosition = (event) => {
-    //      const [positionX, positionY] = getCursorPosition(event);
-    //      positionX = event.clientX - event.target.getBoundingClientRect().x;
-    //      positionY = event.clientY - event.target.getBoundingClientRect().y;
-
-    //     return [positionX, positionY];
-    //   }
-    //   ctx.lineWidth = 3;
-    //   ctx.lineJoin = ctx.lineCap = 'round';
-    //   form.addEventListener('submit', (event) => {
-    //     event.preventDefault();
-    //     const imageURL = canvas.toDataURL();
-    //     const image = document.createElement('img');
-    //     image.src = imageURL;
-    //     image.height = canvas.height;
-    //     image.width = canvas.width;
-    //     image.style.display = 'block';
-    //     form.appendChild(image);
-    //     clearPad();
-    //   })
-    //   const clearPad = () => {
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //   }
-    //   clearButton.addEventListener('click', (event) => {
-    //     event.preventDefault();
-    //     clearPad();
-    //   })
-    state = {trimmedDataURL: null}
-    sigPad = {}
-    clear = () => {
-        this.sigPad.clear()
-    }
-    trim = () => {
-        this.setState({trimmedDataURL: this.sigPad.getTrimmedCanvas()
-            .toDataURL('image.png')})
-         }
-    render()
-    {
-            let {trimmedDataURL} = this.state     
+     
             return (
             <><FormControl>
                 <FormLabel id="demo-radio-buttons-group-label"><b>Consent Form</b></FormLabel>
@@ -120,25 +46,12 @@ export default function RadioButtonsGroup() {
             <title>Signature Pad</title>
             <form class="signature-pad-form" action="#" method="POST">
             <p><b>Signature</b></p>
-            {/* <canvas height="100" width="300" class="signature-pad"></canvas> */}
-            {/* <SignatureCanvas penColor='green'
-    canvasProps={{width: 400, height: 100 className: 'sigCanvas'}} />,
-            <p><a href="#" class="clear-button">Clear</a></p> */}
-            <div className={styles.container}>
-                <div className={styles.sigContainer}>
-                    <SignaturePad canvasProps={{className: styles.sigPad}}
-                    ref={(ref) => { this.sigPad = ref }} />
-                    </div>
-                    <div>
-                        <button className={styles.buttons} onClick={this.clear}> Clear</button>
-                        <button className={styles.buttons} onClick={this.trim}>Trim</button></div>
-                        {trimmedDataURL? <img className={styles.sigImage}src={trimmedDataURL} />: null}
-                        </div>
+            <Signature></Signature>
+            
          </form>
         </FormControl><button type="button" className="btn btn-secondary btn-sm" style={{ cursor: 'pointer', padding: '1%', fontWeight: 'bolder' }} onClick={(e) => { redirecttoConsent(e); } }>Submit</button></>
         
   )
   
 }
-}
-ReactDOM.render(<App />, document.getElementById('root'))
+
