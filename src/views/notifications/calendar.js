64c629088adf864709970f3897a36f54111390e1
Subhangipatro <Subhangipatro999@gmail.com>
2023-05-29 16:48:15 +0530

    calendardate

--- src/views/notifications/calendar.js
+++ src/views/notifications/calendar.js
@@ -7,37 +7,44 @@ import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@
 export default function Calender() {
 
     const [data, setdata] = useState();
-
+    const modifydate = (value, type) => {
+        if (type == 'year') {
+            return value.split('-')[0]
+        } else if (type == 'month') {
+            return value.split('-')[1]
+        } else if (type == 'date') {
+            return value.split('-')[2]
+        }
+    }
     useEffect(() => {
         var requestOptions = {
             method: 'GET',
             redirect: 'follow'
-          };
-          
-          fetch("https://emailnotifications-sh4iojyb3q-uc.a.run.app/", requestOptions)
+        };
+
+        fetch("https://emailnotifications-sh4iojyb3q-uc.a.run.app/", requestOptions)
             .then(response => response.json())
             .then(data => {
                 // Parse the API response to the format expected by the Scheduler component
                 const events = data.map(event => ({
-                  Id: event.Patient_id,
-                  Subject: 'Online meeting with Practitioner '+event.Practitioner_name,
-                  StartTime: event.App_Date,
-                  EndTime: event.App_Date,
-                  Status: 'Incomplete',
-                  Priority: 'High',
-                  Location: "https://meet.google.com/ypu-vavo-riu",
-                  Description:  "Appointment is setup with practitioner "+event.Practitioner_name+". Please join the meeting through this online link: https://meet.google.com/ypu-vavo-riu"
+                    Id: event.Patient_id,
+                    Subject: 'Online meeting with Practitioner ' + event.Practitioner_name,
+                    StartTime: new Date(parseInt(modifydate(event.App_Date, 'year')), parseInt(modifydate(event.App_Date, 'month')), parseInt(modifydate(event.App_Date, 'date')), parseInt(event.Timing), 0),
+                    EndTime: new Date(parseInt(modifydate(event.App_Date,'year')),parseInt(modifydate(event.App_Date,'month')),parseInt(modifydate(event.App_Date,'date')),parseInt(event.Timing)+1,0),
+                    Status: 'Incomplete',
+                    Priority: 'High',
+                    Location: "https://meet.google.com/ypu-vavo-riu",
+                    Description: "Appointment is setup with practitioner " + event.Practitioner_name + ". Please join the meeting through this online link: https://meet.google.com/ypu-vavo-riu"
                 }))
-                setdata(events)
+                setdata({dataSource:events})
+                console.log("event is here", events)
             })
-            .then(result => console.log(result))
             .catch(error => console.log('error', error));
-      }, []);
+    }, []);
 
-      console.log(data)
     return (
         <>
-            <ScheduleComponent height='550px' currentView='Month'  eventSettings={{ dataSource: data }}>
+            <ScheduleComponent height='550px' currentView='Month' eventSettings={data}>
                 <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
             </ScheduleComponent>
         </>
